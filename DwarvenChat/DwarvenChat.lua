85c0b3964a1970ac79c138c5e117528a1cf3de9f
gruzzly-bear <facebawx@gmail.com>
2020-07-20 03:09:02 -0400

    Update DwarvenChat.lua

--- DwarvenChat/DwarvenChat.lua
+++ DwarvenChat/DwarvenChat.lua
@@ -4,7 +4,7 @@
 local ldb = LibStub:GetLibrary("LibDataBroker-1.1");
 local dcBroker = ldb:NewDataObject("DwarvenChat", { 
 	type = "data source",
-	label = "DwarvenChat", 
+	label = "Dwarven Chat", 
 	icon = "Interface\\AddOns\\DwarvenChat\\icon",
 	text = "--"
 	}
@@ -13,17 +13,17 @@ local dcBroker = ldb:NewDataObject("DwarvenChat", {
 
 -- Event Handler
 local function OnEvent(self, event, addOnName)
-	if event == "ADDON_LOADED" and addOnName:lower() == "DwarvenChat" then
+	if event == "ADDON_LOADED" and addOnName:lower() == "dwarvenchat" then
 
 		-- Build our slash commands
 		DwarvenChat_OnLoad()
 
 		-- set saved variables for first time users		
-		if Dwarven_talk_on == nil then
-   			Dwarven_talk_on = 1;
+		if dwarven_talk_on == nil then
+   			dwarven_talk_on = 1;
   		end
-  		if Dwarven_strict_on == nil then
-   			Dwarven_strict_on = 0; 
+  		if dwarven_strict_on == nil then
+   			dwarven_strict_on = 0; 
   		end
 
 		--  Get our speak tables
@@ -33,7 +33,7 @@ local function OnEvent(self, event, addOnName)
 			
 
 		-- set the status text of our LDB object
-		if (Dwarven_talk_on == 1) then
+		if (dwarven_talk_on == 1) then
 			dcBroker.text = "|c0000FF00ON |r"
 		else
 			dcBroker.text = "|c00FF0000OFF|r"
@@ -55,22 +55,22 @@ dcFrame:SetScript("OnEvent", OnEvent);
 function DwarvenChat_OnLoad()
 
 	-- Create our slash commands
-	SlashCmdList["DwarvenChatTOGGLE"] = Dwarven_toggle;
-	SLASH_DwarvenChatTOGGLE1 = "/dwarvenchat";
-	SLASH_DwarvenChatTOGGLE2 = "/dchat";
-	SlashCmdList["DSAY"] = Dwarven_say;
+	SlashCmdList["DWARVENCHATTOGGLE"] = dwarven_toggle;
+	SLASH_DWARVENCHATTOGGLE1 = "/dwarvenchat";
+	SLASH_DWARVENCHATTOGGLE2 = "/dchat";
+	SlashCmdList["DSAY"] = dwarven_say;
 	SLASH_DSAY1 = "/ds";
 	SLASH_DSAY2 = "/dsay";
-	SlashCmdList["DYELL"] = Dwarven_yell;
-	SLASH_PYELL1 = "/dy";
-	SLASH_PYELL2 = "/dyell";
-	SlashCmdList["DPARTY"] = Dwarven_party;
-	SLASH_PPARTY1 = "/dp";
-	SLASH_PPARTY2 = "/dparty";
-	SlashCmdList["DGUILD"] = Dwarven_guild;
-	SLASH_PGUILD1 = "/dg";
-	SLASH_PGUILD2 = "/dguild";
-	SlashCmdList["DSTRICTTOGGLE"] = Dwarven_strict;
+	SlashCmdList["DYELL"] = dwarven_yell;
+	SLASH_DYELL1 = "/dy";
+	SLASH_DYELL2 = "/dyell";
+	SlashCmdList["DPARTY"] = dwarven_party;
+	SLASH_DPARTY1 = "/dp";
+	SLASH_DPARTY2 = "/dparty";
+	SlashCmdList["DGUILD"] = dwarven_guild;
+	SLASH_DGUILD1 = "/dg";
+	SLASH_DGUILD2 = "/dguild";
+	SlashCmdList["DSTRICTTOGGLE"] = dwarven_strict;
 	SLASH_DSTRICTTOGGLE1 = "/dstrict";
 
 	-- announce addon load
@@ -81,7 +81,7 @@ end
 
 
 -- Blizzard function to be hooked
-local DwarvenChat_SendChatMessage = SendChatMessage;
+local dwarvenChat_SendChatMessage = SendChatMessage;
 
 -- Wintertime and Dwarf Accent conflict.
 -- If we detect Wintertime we'll note the channel and stay off of it
@@ -89,37 +89,37 @@ local wtID, wtName = GetChannelName("WinterTimeGlobal")
 
 function SendChatMessage(msg, system, language, chatnumber) 
 	--DEFAULT_CHAT_FRAME:AddMessage("Channel ID: "..chatnumber.." message: "..msg.." ChannelName: "..wtID .. ", " .. wtName);
-    if (Dwarven_talk_on == 1 and msg ~= "" and (string.find(msg, "%[") == nil)) then 
+    if (dwarven_talk_on == 1 and msg ~= "" and (string.find(msg, "%[") == nil)) then 
 	if ( string.find(msg, "^%/") == nil ) and (chatnumber ~= wtID) then
-          msg = DwarvenChat(msg);
+          msg = dwarvenchat(msg);
 	end
     end
-    DwarvenChat_SendChatMessage(msg, system, language, chatnumber);
+    dwarvenChat_SendChatMessage(msg, system, language, chatnumber);
 end 
 
 
-function prepend_Dwarven(inputString)
-	local phrase_array = speakDB["DwarvenChat_PrependDB"]
-	if (Dwarven_strict_on == 0) then
+function prepend_dwarven(inputString)
+	local phrase_array = speakDB["dwarvenChat_PrependDB"]
+	if (dwarven_strict_on == 0) then
 		inputString = phrase_array[math.random(table.getn(phrase_array))]..inputString
 	end
 	return inputString
 end
 
-function append_Dwarven(inputString)
- 	local phrase_array = speakDB["DwarvenChat_AppendDB"]
-	if (Dwarven_strict_on == 0) then
+function append_dwarven(inputString)
+ 	local phrase_array = speakDB["dwarvenChat_AppendDB"]
+	if (dwarven_strict_on == 0) then
 		inputString = string.gsub(inputString, '([%.%!%?])', phrase_array[math.random(table.getn(phrase_array))].."%1",1)
 	end 
 	return inputString
 end
 
-function sub_Dwarven(inputString)
+function sub_dwarven(inputString)
 
 	local cur_sub = {}
-	local sub_array = speakDB["DwarvenChat_ReplaceDB"]
+	local sub_array = speakDB["dwarvenChat_ReplaceDB"]
 	
-	inputString = string.gsub(inputString, "(%s)", inject_Dwarven)
+	inputString = string.gsub(inputString, "(%s)", inject_dwarven)
 
 	for i = 1, table.getn( sub_array ) do
 		cur_sub['o']=sub_array[i]['o']
@@ -138,65 +138,65 @@ function sub_Dwarven(inputString)
 	return inputString
 end
 
-function inject_Dwarven(inputString)
-	if ( math.random(100) > 98 and Dwarven_strict_on == 0) then
+function inject_dwarven(inputString)
+	if ( math.random(100) > 98 and dwarven_strict_on == 0) then
 		inputString = ", Ach, "
 	end
 	return inputString
 end
 
-function DwarvenChat(x)
-	x = sub_Dwarven(x)
+function dwarvenchat(x)
+	x = sub_dwarven(x)
 	if (math.random(100) > 75 ) then
-		x = append_Dwarven(x)
+		x = append_dwarven(x)
 	else
-		if(math.random(100) > 50 and Dwarven_strict_on == 0) then
+		if(math.random(100) > 50 and dwarven_strict_on == 0) then
 			x = x .. " Bwaha!"
 		end
 	end
 	if ( math.random(100) > 75 ) then
-		x = prepend_Dwarven(x)
+		x = prepend_dwarven(x)
 	end
 	return x;
 end
 
-function Dwarven_toggle(toggle)
+function dwarven_toggle(toggle)
 	if ( toggle == "on" ) then
-		Dwarven_talk_on = 1
+		dwarven_talk_on = 1
 		dcBroker.text = "|c0000FF00ON|r"
 		DEFAULT_CHAT_FRAME:AddMessage("Dwarf Accent On")
 	elseif ( toggle == "off" ) then
-		Dwarven_talk_on = 0
+		dwarven_talk_on = 0
 		dcBroker.text = "|c00FF0000OFF|r"
 		DEFAULT_CHAT_FRAME:AddMessage("Dwarf Accent off")
 	else	
-		DEFAULT_CHAT_FRAME:AddMessage(Dwarven_helptext(helptext))
+		DEFAULT_CHAT_FRAME:AddMessage(dwarven_helptext(helptext))
 	end
 end
 
-function Dwarven_strict(toggle)
+function dwarven_strict(toggle)
 	if ( toggle == "on" ) then
-		Dwarven_strict_on = 1
-		DEFAULT_CHAT_FRAME:AddMessage("DwarvenChat append, prepend and inject phrases are off")
+		dwarven_strict_on = 1
+		DEFAULT_CHAT_FRAME:AddMessage("Dwarven Chat append, prepend and inject phrases are off")
 	elseif ( toggle == "off" ) then
-		Dwarven_strict_on = 0
-		DEFAULT_CHAT_FRAME:AddMessage("DwarvenChat append, prepend and inject phrases are on")
+		dwarven_strict_on = 0
+		DEFAULT_CHAT_FRAME:AddMessage("Dwarven Chat append, prepend and inject phrases are on")
 	else
 		helptext = ""
-		DEFAULT_CHAT_FRAME:AddMessage(Dwarven_helptext(helptext))
+		DEFAULT_CHAT_FRAME:AddMessage(dwarven_helptext(helptext))
 	end
 end
 
-function Dwarven_helptext(helptext)
+function dwarven_helptext(helptext)
 	helptext = "/dchat (" 
-	if (Dwarven_talk_on == 1) then
+	if (dwarven_talk_on == 1) then
 		helptext = helptext .. "|c0000FF00on|r|off)"
 	else
 		helptext = helptext .. "on| |c00FF0000off|r)"
 	end
-	helptext = helptext .. ": toggle global Dwarven talk\n/ds,/dsay: say something in Dwarven\n/dy,/dyell: yell something in Dwarven\n/dp,/dparty: party talk something in Dwarven\n/dg, /dguild: guildtalk in Dwarven\n/"
+	helptext = helptext .. ": toggle global dwarven talk\n/ds,/dsay: say something in Dwarven\n/dy,/dyell: yell something in Dwarven\n/dp,/dparty: party talk something in Dwarven\n/dg, /dguild: guildtalk in Dwarven\n/"
 	helptext = helptext .. "dstrict ("
-	if (Dwarven_strict_on == 1) then
+	if (dwarven_strict_on == 1) then
 		helptext = helptext .. "|c0000FF00on|r|off)"
 	else
 		helptext = helptext .. "on| |c00FF0000off|r)"
@@ -206,23 +206,23 @@ function Dwarven_helptext(helptext)
 end
 
 
-function Dwarven_say(x)
-	x = DwarvenChat(x)
+function dwarven_say(x)
+	x = dwarvenchat(x)
 	SendChatMessage(x);
 end
 
-function Dwarven_yell(x)
-	x = DwarvenChat(x)
+function dwarven_yell(x)
+	x = dwarvenchat(x)
 	SendChatMessage(x,"YELL");
 end
 
-function Dwarven_party(x)
-	x = DwarvenChat(x)
+function dwarven_party(x)
+	x = dwarvenchat(x)
 	SendChatMessage(x,"PARTY");
 end
 
-function Dwarven_guild(x)
-	x = DwarvenChat(x)
+function dwarven_guild(x)
+	x = dwarvenchat(x)
 	SendChatMessage(x,"GUILD");
 end
 
@@ -230,10 +230,10 @@ end
 
 -- Create the LDB tooltip
 function dcBroker:OnTooltipShow()
-	statusLine = "DwarvenChat is "
-	if (Dwarven_talk_on == 1) then
+	statusLine = "Dwarven Chat is "
+	if (dwarven_talk_on == 1) then
 		statusLine = statusLine .. "|c0000FF00ON|r"
-		if (Dwarven_strict_on == 1) then
+		if (dwarven_strict_on == 1) then
 			statusLine = statusLine .. " (strict mode)"			
 		else
 			statusLine = statusLine .. " (verbose mode)"
@@ -242,7 +242,7 @@ function dcBroker:OnTooltipShow()
 		statusLine = statusLine .. "|c00FF0000OFF|r"
 	end 
 	self:AddLine(statusLine);
-	self:AddLine("Left Click toggles DwarvenChat on and off", 1, 1, 1);
+	self:AddLine("Left Click toggles Dwarven Chat on and off", 1, 1, 1);
     	self:AddLine("Right Click toggles betrween strict and verbose", 1, 1, 1);
     	self:AddLine(" ", 1, 1, 1);
     	self:AddLine("type: /dchat for command line options", 1, 1, 1);
@@ -253,18 +253,18 @@ end
 -- Toggles functions based on LDB button clicks
 function dcBroker:OnClick(button)
 	if button== "LeftButton" then
-		if (Dwarven_talk_on == 1) then
-			Dwarven_toggle("off")
+		if (dwarven_talk_on == 1) then
+			dwarven_toggle("off")
 			dcBroker.text = "|c00FF0000OFF|r"
 		else
-			Dwarven_toggle("on")
+			dwarven_toggle("on")
 			dcBroker.text = "|c0000FF00ON |r"
 		end
 	elseif button== "RightButton" then
-		if (Dwarven_strict_on == 1) then
-			Dwarven_strict("off")
+		if (dwarven_strict_on == 1) then
+			dwarven_strict("off")
 		else
-			Dwarven_strict("on")
+			dwarven_strict("on")
 		end
 	end
 end
@@ -272,13 +272,13 @@ end
 -- Create default speak tables
 
 function CreateSpeakDB()
-	local DwarvenChat_PrependDB = {
+	local dwarvenChat_PrependDB = {
 	"Aye, ",
 	}
-	local DwarvenChat_AppendDB = {
+	local dwarvenChat_AppendDB = {
 	", Bwaha!",
 	}
-	local DwarvenChat_ReplaceDB = {
+	local dwarvenChat_ReplaceDB = {
 	{o={"^hello","^hiya","^hi there", "^hey"}, r={"Well met","E'llo"}},
 	{o={"no", "nah"}, r={"nae"}},
 	{o={"^no", "^nah"}, r={"^nae"}},
@@ -305,9 +305,9 @@ function CreateSpeakDB()
 
 	speakDB = {}	
 
-	speakDB["DwarvenChat_PrependDB"] = DwarvenChat_PrependDB
-	speakDB["DwarvenChat_AppendDB"] = DwarvenChat_AppendDB
-	speakDB["DwarvenChat_ReplaceDB"] = DwarvenChat_ReplaceDB
+	speakDB["dwarvenChat_PrependDB"] = dwarvenChat_PrependDB
+	speakDB["dwarvenChat_AppendDB"] = dwarvenChat_AppendDB
+	speakDB["dwarvenChat_ReplaceDB"] = dwarvenChat_ReplaceDB
 
 
 	DEFAULT_CHAT_FRAME:AddMessage("speakDB initialized");
