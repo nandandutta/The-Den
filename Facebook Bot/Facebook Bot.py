9dfb9de292fc138f4c5a4b8bbb2b1ef408e154d7
Gruzzly-bear <MB.Bowen@outlook.com>
2020-09-27 10:55:55 -0400

    update

--- Facebook Bot/Facebook Bot.py
+++ Facebook Bot/Facebook Bot.py
@@ -6,16 +6,15 @@ from configparser import ConfigParser
 from time import sleep
 config = configparser.ConfigParser()
 parser = ConfigParser()
-parser.read("settings.cfg")
-#Get config file from the executing directory.
-config.read(os.path.join(os.path.dirname(__file__),"settings.cfg"))
-ACCESS_TOKEN=config.get('settings','ACCESS_TOKEN')
+parser.read("settings.cfg")  #Get config file from the executing directory.
+config.read(os.path.join(os.path.dirname(__file__),"settings.cfg")) # Reads from the config file.
+ACCESS_TOKEN=config.get('settings','ACCESS_TOKEN') # Pulls the token from the file and uses it in the code.
 sleep_time = int(config.get("settings", "SLEEP")) # sleep for 1 second by default
 
 while True:
 	graph = GraphAPI(ACCESS_TOKEN)
-	graph.post('me/feed', message=(random.choice(list(open('text.txt')))))
+	graph.post('me/feed', message=(random.choice(list(open('text.txt'))))) # Chooses a random text option to post to the facebook wall.
 
 
-	print("Your post was posted!");
+	print("Your post was posted!"); # Tells you that your post was posted via console.
 	sleep(sleep_time)
